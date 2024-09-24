995ee047874f37901ed3e183cb7603f682889ead
JoaquimARM <joaquimarm13@gmail.com>
2024-09-24 09:57:57 -0300

    ola

--- src/main/java/com/senai/sesi/Tutorial_crud/controller/usuarioController.java
+++ src/main/java/com/senai/sesi/Tutorial_crud/controller/usuarioController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.senai.sesi.Tutorial_crud.model.usuario;
 import com.senai.sesi.Tutorial_crud.repository.UsuarioRepository;
 
 
@@ -13,10 +17,29 @@ public class usuarioController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	@GetMapping("/formulario")
+	public String mostrarFormulario(Model modelo) {
+		modelo.addAttribute("usuario", new usuario());
+		return "formulario";
+	}
+	
+	@PostMapping("/salvarUsuario")
+	public String salvarUsuario(@ModelAttribute usuario usuario) {
+		usuarioRepository.save(usuario);
+		return "redirect:/usuario";
+		
+	}
+	
 	@GetMapping("/usuario")
 	public String listarUsuario(Model modelo) {
 		modelo.addAttribute("usuarios",usuarioRepository.findAll());
  		return "usuario";
+	}
+	
+	@GetMapping("removerUsuario/{id}")
+	public String removerUsuario(@PathVariable int id) {
+		usuarioRepository.deleteById(id);
+		return "redirect:/usuario";
 	}
 	
 	
